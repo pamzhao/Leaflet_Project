cdc7418f72cfbd8dc1acf63fd94955f6cb5fa0ed
Pamela Zhao <pamzhao@yahoo.com>
2019-06-16 00:13:29 -0700

    Add color to the map.

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,31 +9,52 @@ var myMap = L.map('map').setView([37.8, -96], 4);
     accessToken: API_KEY
   }).addTo(myMap);
 
-  var mapStyle = {
-    color: "white",
-    fillColor: "red",
-    fillOpacity: 0.3,
-    weight: 1.5
-  };
+  // var mapStyle = {
+  //   color: "white",
+  //   fillColor: "red",
+  //   fillOpacity: 0.3,
+  //   weight: 1.5
+  // };
 
-
+  function setColor(rate) {
+    if (rate > 0 && rate < 10) {
+        return "green";
+    } else if (rate >= 10 && rate < 20) {
+      return "yellow";
+    } else if (rate >= 20 && rate < 30) {
+      return "orange";
+    } else if (rate >= 30 && rate < 40) {
+      return "red";
+    } else if (rate >= 40 && rate < 50) {
+    return "red";
+  } else {
+      return "black";
+    }
+  }
   
   // Read the data.geojson file
   d3.json("data.geojson", function(data) {
     console.log(data);
     L.geoJson(data, {
-      style: mapStyle,
+      style: function(feature) {
+        return {
+          color: "white",
+          fillColor: setColor(feature.properties.Obesity),
+          fillOpacity: 0.5,
+          weight: 1.5
+        };
+      },
       onEachFeature: function(feature, layer) {
         layer.on({
           mouseover: function(event) {
             layer = event.target;
-            layer.setStyle({fillOpacity: 0.5});
+            layer.setStyle({fillOpacity: 0.8});
             layer.bindPopup("<h1>" + feature.properties.NAME + "</h1><hr><h2>" + feature.properties.Obesity + "% Obesity</h2>");
           },
           // Reset in mouse out
           mouseout: function(event) {
             layer = event.target;
-            layer.setStyle({fillOpacity: 0.3});
+            layer.setStyle({fillOpacity: 0.5});
           }
         })
       }
